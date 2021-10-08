8510b68c11ee91f992d4c5b1c91ebaf9f86502d1
Savar Jain: HomeLaptop <jainsavar@gmail.com>
2021-10-08 19:55:11 -0400

    Added note rendering with mapping components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
-      <Note />
+      {notes.map((note) => (
+        <Note title={note.title} content={note.content} key={note.key} />
+      ))}
       <Footer />
     </div>
   );
