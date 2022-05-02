5df51aa680def22e89aadae46c8662456866ddad
Savar Jain: School Laptop <jainsavar@gmail.com>
2022-05-02 12:00:25 -0400

    Make all api routes

---
+++ api/api.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(express.json());
+app.use(methodOverride("_method"));
+
+const Note = mongoose.model("Note", require("./Note.model"));
+
+app.get("/notes", async (req, res) => {
+  try {
+    const notes = await Note.find({});
+    res.json({ data: notes, statusCode: 200 });
+  } catch (err) {
+    res.json({ data: err.message, statusCode: 500 });
+  }
+});
+
+app.post("/notes", async (req, res) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  const newNote = new Note({ title, content });
+  try {
+    await newNote.save();
+    res.json({ data: newNote, statusCode: 201 });
+  } catch (err) {
+    res.json({ data: err.message, statusCode: 500 });
+  }
+});
+
+app.get("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const note = await Note.findById(id);
+    res.json({ data: note, statusCode: 200 });
+  } catch (err) {
+    res.json({ data: err.message, statusCode: 500 });
+  }
+});
+
+app.put("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  const title = req.body.title;
+  const content = req.body.content;
+  try {
+    const note = await Note.findById(id);
+    note.title = title;
+    note.content = content;
+    await note.save();
+    res.json({ data: note, statusCode: 200 });
+  } catch (err) {
+    res.json({ data: err.message, statusCode: 500 });
+  }
+});
+
+app.delete("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const note = await Note.findById(id);
+    await Note.deleteOne({ _id: note.id });
+    res.json({ data: note, statusCode: 200 });
+  } catch (err) {
+    res.json({ data: err.message, statusCode: 500 });
+  }
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server started on port 5000");
+});
