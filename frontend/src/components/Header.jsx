e2c2f1389032cd756f168af1f17e09ce18c20648
Savar Jain: School Laptop <jainsavar@gmail.com>
2022-05-02 17:46:50 -0400

    Fix mobile responsive bug

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div>
       <header>
-        <HighlightIcon style={{ paddingRight: "10px" }} />
+        <HighlightIcon style={{ paddingRight: "10px", maxWidth: "25px" }} />
         <h1 style={{ display: "inline" }}>Keeper</h1>
       </header>
     </div>
